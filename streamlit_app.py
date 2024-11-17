096240ffdda5ba886c28f33b7606512df0269fad
Oviya Gnanasekar <114509900+Oviya1330@users.noreply.github.com>
2024-11-16 23:58:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,10 @@ with st.sidebar:
 with st.expander('Sample'):
   st.write('**input_penguins**')
   input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander('Input features'):
+  st.write('**Input penguin**')
   input_df
+  st.write('**Combined penguins data**')
+  input_penguins
