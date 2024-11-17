9753b9d2ec6344d159a307272f5fb54d39ec3258
Oviya Gnanasekar <114509900+Oviya1330@users.noreply.github.com>
2024-11-16 23:22:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('ML app 😒')
 st.info('This is a ML app')
 
 with st.expander('Data'):
-  st.write(**Raw data**)
+  st.write('**Raw data**')
   
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
