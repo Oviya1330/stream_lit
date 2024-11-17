de795450402b5bad36de1cbb90a9ee3f0a55b494
Oviya Gnanasekar <114509900+Oviya1330@users.noreply.github.com>
2024-11-17 14:45:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 target = lambda p:target_mapper[p]
-st.write(target_mapper('Chinstrap'))
+st.write(target('Chinstrap'))
   
 
 with st.expander('Input features'):
