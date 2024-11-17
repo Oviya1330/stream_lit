cf6dee21b09d11557075506822ee404ed3e788d2
Oviya Gnanasekar <114509900+Oviya1330@users.noreply.github.com>
2024-11-17 00:00:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,7 @@ with st.sidebar:
         'body_mass_g': body_mass_g,
         'sex': gender}
 
-with st.expander('Sample'):
-  st.write('**input_penguins**')
+
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X], axis=0)
 
