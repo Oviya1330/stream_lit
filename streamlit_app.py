d52aecc795d5efa4696dd1e83db5bf30885bb8dc
Oviya Gnanasekar <114509900+Oviya1330@users.noreply.github.com>
2024-11-16 23:53:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,15 @@ with st.sidebar:
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
   gender = st.selectbox('sex',('male','female'))
   bill_length_mm = st.slider('bill_length_mm', 32.1,59.6,43.9)
+
+    data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+
+with st.expander('Sample'):
+  st.write('**input_penguins**')
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
